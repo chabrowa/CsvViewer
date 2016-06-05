719a40f40d87d0401988a59eb6f6f31712d65c90
Emilia Kacprzak <elessarin@gmail.com>
2016-06-05 12:08:28 +0100

    new module

--- src/DataManager.js
+++ src/DataManager.js
@@ -3,10 +3,6 @@ import { connect } from 'react-redux'
 import styles from './app.css'
 
 const DataManager = React.createClass({
-  // propTypes: {
-  //   files: React.PropTypes.array
-  // },
-
   getInitialState () {
     return {
       headerIndex: 0
@@ -23,29 +19,11 @@ const DataManager = React.createClass({
     return (
       <div className={ styles.main }>
         <label>
-          //<input onChange={this.handleClickChangeMode} type='checkbox' />Show Text
           NANANANANNANAA BATMAN
         </label>
-        // {Object.keys(files).map((fileName) =>
-        //   <div key={fileName}>
-        //    <h2>{fileName}</h2>
-        //     <span>
-        //       <FileTable fileName={fileName} files={files} mode={mode} />
-        //     </span>
-        //   </div>)
-        // }
       </div>
     )
   }
 })
 
-// const mapStateToProps = (state) => {
-//   return {
-//     files: getFiles(state)
-//   }
-// }
-//
-// export default connect(
-//   mapStateToProps,
-//   null
-// )(FilesViewer)
+ export default DataManager
